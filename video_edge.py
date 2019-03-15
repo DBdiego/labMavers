effb5634c180cd11e5398a68b59a72b0d694a3fa
sbahnam <stavrowbahnam@gmail.com>
2019-03-15 10:46:00 +0100

    some_little_changes

--- video_edge.py
+++ video_edge.py
@@ -9,9 +9,13 @@ import numpy as np
 import cv2
 import pdb
 from scipy import signal
+import imutils
 
 
+#YOLO can be used foor deep nets; cnn
 
+
+kernel = np.ones((20, 20), np.uint8)
 vidcap = cv2.VideoCapture('./Videos/zoovideo3.mp4')
 
 while True:
@@ -19,32 +23,64 @@ while True:
     if not success:
         break
 
+    small = cv2.resize(frame, (0,0), fx=0.5, fy=0.5) 
+#    frame = small
+    compress = 1
     edges = cv2.Canny(frame, 100, 200)
 
+    closing_grass = cv2.morphologyEx(frame, cv2.MORPH_CLOSE, kernel)
     
-    grassbool1 =  frame[:,:,1] < 220
-    grassbool2 = frame[:,:,1] > 120
-    grassbool3 = frame[:,:,0] < 200
-    grassbool4 = frame[:,:,2] < 200
-    grassbool5 = frame[:,:,0] > 60
-    grassbool6 = frame[:,:,2] > 90
+    grassbool1 =  closing_grass[:,:,1] < 220
+    grassbool2 = closing_grass[:,:,1] > 120
+    grassbool3 = closing_grass[:,:,0] < 200
+    grassbool4 = closing_grass[:,:,2] < 200
+    grassbool5 = closing_grass[:,:,0] > 60
+    grassbool6 = closing_grass[:,:,2] > 90
     grass = (grassbool1*grassbool2*grassbool3*grassbool4*grassbool5*grassbool6*255).astype(np.uint8)
+
 
 
     boolarray = edges > 100
     boolgrass = grass > 250
-    shift = 50
-    zeros_array = np.zeros((shift,640)).astype(int)
+    shift = 20
+    zeros_array = np.zeros((shift,int(640/compress))).astype(int)
     bool_object = boolarray*1
     bool_grass = (grass > 250)*1
     test1_array = np.concatenate(( zeros_array, bool_grass), axis=0)
     test2_array = np.concatenate((bool_object, zeros_array), axis =0)
     grass_over = (test1_array*test2_array*255).astype(np.uint8)
-    over_grass = np.delete(grass_over, range(352,352+shift), 0)
+    over_grass = np.delete(grass_over, range(int(352/compress),352+shift), 0)
 
     new_edges = (edges - over_grass).astype(np.uint8)
     
+#    cnts = cv2.findContours(new_edges.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+#    cnts = imutils.grab_contours(cnts)
+#    cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:]
+#    screenCnt = None
+##    print('wat')
+#    for c in cnts:
+#        peri = cv2.arcLength(c,True)
+#        approx = cv2.approxPolyDP(c, 0.015 * peri, True)
+##        print('huh')
+#        if len(approx) == 4:
+#            screenCnt = approx
+#            print('test')
+#            break
+
+
+#    cv2.drawContours(frame, [screenCnt], -1, (0, 255, 0), 3)
+
+#    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#    dst = cv2.cornerHarris(gray, 2, 3, 0.04)
+#    dst = cv2.dilate(dst, None)
     
+#    frame[dst > 0.01*dst.max()] = [0,0,255]
+#    cv2.imshow('dst', frame)
+
+
+    # interesting, but perhaps decreasing size image would have same effect
+    closing = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, kernel)
+
 #    box = 30
 #    for i in range(int(255/box)+1):
 #        boolgray1 = gray>(255-box*(i+1))
@@ -61,17 +97,10 @@ while True:
 ##    gray[:,i_s] = 255
 #    new_gray = gray - clean_edges
 
-    cv2.imshow('frame', new_edges)
-    cv2.waitKey(1)
+    cv2.imshow('frame', closing)
+    cv2.waitKey(30)
     
     if cv2.waitKey(1) and 0xFF == ord('q'):
         break
     
-small = np.zeros((80,80)).astype(np.uint8)
-#for i in range(len(small[:,1])): Do this with numpy instead, quicker
-#    for j in range(640):
-#         som += sum(gray[4*i:4*i+4, j]).astype(np.uint16)
-#         if (j+ 1) % 8 == 0 and j != 0:
-#             small[i,int((j+1)/8-1)] = som/32
-#             som = 0
 
